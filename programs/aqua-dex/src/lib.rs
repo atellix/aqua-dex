1db9eea51099004e8686be7df63daac13a70bdaa
Mike Frager <mfrager@gmail.com>
2021-09-12 14:51:24 -0700

    Start create_market()

--- programs/aqua-dex/src/lib.rs
+++ programs/aqua-dex/src/lib.rs
@@ -16,6 +16,8 @@ use crate::slab_alloc::{ SlabPageAlloc, CritMapHeader, CritMap, AnyNode, LeafNod
 
 pub const MAX_ORDERS: u32 = 128;    // Max orders on each side of the orderbook
 pub const MAX_ACCOUNTS: u32 = 256;  // Max number of accounts per settlement data file
+pub const SPL_TOKEN_PK = "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA";
+pub const ASC_TOKEN_PK = "ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL";
 
 #[repr(u8)]
 #[derive(PartialEq, Debug, Eq, Copy, Clone)]
@@ -110,6 +112,47 @@ fn verify_matching_accounts(left: &Pubkey, right: &Pubkey, error_msg: Option<Str
     Ok(())
 }
 
+fn create_associated_token_account(
+    funder: &AccountInfo,
+    account: &AccountInfo,
+    owner: &AccountInfo,
+    mint: &AccountInfo,
+    system_program: &AccountInfo,
+    spl_token_program: &AccountInfo,
+    rent: &AccountInfo,
+) -> ProgramResult {
+    let instr = Instruction {
+        program_id: asc_token,
+        accounts: vec![
+            AccountMeta::new(*funder.key, true),
+            AccountMeta::new(*account.key, false),
+            AccountMeta::new_readonly(*owner.key, false),
+            AccountMeta::new_readonly(*mint.key, false),
+            AccountMeta::new_readonly(*system_program.key, false),
+            AccountMeta::new_readonly(*spl_token_program.key, false),
+            AccountMeta::new_readonly(*rent.key, false),
+        ],
+        data: vec![],
+    };
+    let res = invoke(
+        &instr,
+        &[
+            funder.clone(),
+            account.clone(),
+            owner.clone(),
+            mint.clone(),
+            system.clone(),
+            spl_token_program.clone(),
+            rent.clone(),
+        ]
+    );
+    if res.is_err() {
+        msg!("Create associated token failed");
+        return Err(ErrorCode::InvalidAccount.into());
+    }
+    Ok(())
+}
+
 #[program]
 pub mod aqua_dex {
     use super::*;
@@ -117,6 +160,37 @@ pub mod aqua_dex {
     pub fn create_market(ctx: Context<CreateMarket>,
         inp_param: u64,
     ) -> ProgramResult {
+        let acc_market = &ctx.accounts.market.to_account_info();
+        let acc_state = &ctx.accounts.state.to_account_info();
+        let acc_agent = &ctx.accounts.agent.to_account_info();
+        let acc_mkt_mint = &ctx.accounts.mkt_mint.to_account_info();
+        let acc_mkt_vault = &ctx.accounts.mkt_vault.to_account_info();
+        let acc_prc_mint = &ctx.accounts.prc_mint.to_account_info();
+        let acc_prc_vault = &ctx.accounts.prc_vault.to_account_info();
+
+        // Verify market agent
+        let acc_root_expected = Pubkey::create_program_address(&[ctx.program_id.as_ref(), &[inp_root_nonce]], ctx.program_id)
+            .map_err(|_| ErrorCode::InvalidDerivedAccount)?;
+        verify_matching_accounts(acc_root.key, &acc_root_expected, Some(String::from("Invalid root data")))?;
+
+        let spl_token: Pubkey = Pubkey::from_str(SPL_TOKEN_PK).unwrap();
+        let asc_token: Pubkey = Pubkey::from_str(ASC_TOKEN_PK).unwrap();
+
+        // Verify associated token (market)
+        let derived_mkt_vault = Pubkey::create_program_address(
+            &[
+                &acc_root.key.to_bytes(),
+                &spl_token.to_bytes(),
+                &acc_mint.key.to_bytes(),
+                &[inp_tokn_nonce]
+            ],
+            &asc_token
+        ).map_err(|_| ErrorCode::InvalidDerivedAccount)?;
+        if derived_key != *acc_tokn.key {
+            msg!("Invalid token account");
+            return Err(ErrorCode::InvalidDerivedAccount.into());
+        }
+
         Ok(())
     }
 
@@ -145,6 +219,8 @@ pub struct CreateMarket<'info> {
     pub market: AccountInfo<'info>,
     #[account(mut)]
     pub state: AccountInfo<'info>,
+    #[account(mut)]
+    pub agent: AccountInfo<'info>,
     pub mkt_mint: AccountInfo<'info>,
     #[account(mut)]
     pub mkt_vault: AccountInfo<'info>,
@@ -164,6 +240,8 @@ pub struct Market {
     pub active: bool,                   // Active flag
     pub order_fee: u64,                 // Fee to reserve space in a settlement log when an order is filled or evicted
     pub state: Pubkey,                  // Market statistics (frequently updated market details)
+    pub agent: Pubkey,                  // Market program derived address for signing transfers
+    pub manager: Pubkey,                // Market manager
     pub mkt_mint: Pubkey,               // Token mint for market tokens (Token A)
     pub mkt_vault: Pubkey,              // Vault for Token A (an associated token account controlled by this program)
     pub prc_mint: Pubkey,               // Token mint for pricing tokens (Token B)
@@ -184,6 +262,11 @@ pub struct MarketState {
     pub prc_vault_balance: u64,         // Token B vault total balance
     pub prc_order_balanae: u64,         // Token B order balance
     pub last_ts: i64,                   // Timestamp of last event
+}
+
+#[account]
+pub struct MarketAgent {
+    pub created: bool,
 }
 
 #[error]
